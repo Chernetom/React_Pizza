f8d234d97281a483a41aff6eab14a590efe91922
Chernotom <b3rberserker@yandex.ru>
2023-02-14 22:03:41 +0300

    async fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,32 +5,38 @@ import PizzaBlock from "../modules/PizzaBlock/PizzaBlock";
 import {useContext, useEffect, useState} from "react";
 import Paginator from "../modules/Paginator/Paginator";
 import {SearchContext} from "../App";
-import {useSelector} from "react-redux";
-import axios from 'axios';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchPizzas} from "../redux/slices/pizzaSlice";
 
 const Home = () => {
+    const dispatch = useDispatch();
+
+    const {items, status} = useSelector(state => state.pizza);
     const {categoryId, sortName } = useSelector(state => state.filter);
 
     const {searchValue} = useContext(SearchContext);
-    const [items,setItems] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
     const [currentPage,setCurrentPage] = useState(1);
 
     useEffect(() => {
-        setIsLoading(true);
-        const category = categoryId > 0 ? `category=${categoryId}` : '';
-        const sortBy = sortName.sortProperty.replace('-','');
-        const order = sortName.sortProperty.includes('-') ? 'asc' : 'desc';
-        const search = searchValue ? `search=${searchValue}` : '';
+        const getPizzas = async () => {
+            const category = categoryId > 0 ? `category=${categoryId}` : '';
+            const sortBy = sortName.sortProperty.replace('-','');
+            const order = sortName.sortProperty.includes('-') ? 'asc' : 'desc';
+            const search = searchValue ? `search=${searchValue}` : '';
 
-        axios
-            .get(`https://63d67d25dc3c55baf43711e8.mockapi.io/items?page=${currentPage}&limit=4&${category}&${search}&sortBy=${sortBy}&order=${order}`)
-            .then(response => {
-                setItems(response.data);
-                setIsLoading(false);
-            })
+            dispatch(
+                fetchPizzas({
+                    currentPage,
+                    category,
+                    search,
+                    sortBy,
+                    order
+                })
+            );
 
             window.scrollTo(0, 0);
+        }
+        getPizzas();
     },[categoryId, sortName, searchValue, currentPage]);
 
     return (
@@ -41,9 +47,13 @@ const Home = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-                {isLoading
-                    ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                    : items.map(p => <PizzaBlock key={p.id} {...p}  />)}
+                {status === 'error'
+                    ? <div>Произошла ошибка </div>
+                    :   status === 'loading'
+                        ? [...new Array(4)].map((_, index) => <Skeleton key={index} />)
+                        : items.map(p => <PizzaBlock key={p.id} {...p}  />)
+                }
+
             </div>
             <Paginator currentPage={currentPage} setCurrentPage={setCurrentPage}/>
         </div>
